ae62a3cbd1a0695ac10a5580d6715e6ea4e58c26
Alimpp <86446834+alimpp@users.noreply.github.com>
2025-08-30 12:14:58 +0330

    Update otp.controller.ts

--- src/otp/otp.controller.ts
+++ src/otp/otp.controller.ts
@@ -3,10 +3,12 @@ import { OtpService } from './otp.service';
 
 @Controller('otp')
 export class OtpController {
+  
   constructor(private readonly otpService: OtpService) {}
 
   @Post('request')
   async requestOtp(@Body('phoneNumber') phoneNumber: string) {
     return this.otpService.generateOtp(phoneNumber);
   }
+  
 }
