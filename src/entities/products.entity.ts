c944b860be4545819403a095ca3efac4c24ae134
Alimp <A.Mohsenpour@sgas.ir>
2025-10-05 10:33:00 +0330

    Add products entity

---
+++ src/entities/products.entity.ts
@@ -0,0 +1,71 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class ProductsEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column()
+  submiter: string;
+  @Column()
+  name: string;
+  @Column()
+  description: string;
+  @Column({ type: 'jsonb', nullable: true })
+  category: Array<{
+    id: string;
+    name: string;
+    imageId: string;
+    iconId: string;
+  }>;
+  @Column({ type: 'jsonb', nullable: true })
+  property: Array<{
+    id: string;
+    name: string;
+  }>;
+  @Column({ type: 'jsonb', nullable: true })
+  property_value: Array<{
+    id: string;
+    name: string;
+    properttyId: string;
+  }>;
+  @Column({ type: 'jsonb', nullable: true })
+  tags: Array<{
+    name: string;
+  }>;
+  @Column({ type: 'jsonb', nullable: true })
+  relatedBlogs: Array<{
+    blogId: string;
+    title: string;
+    subTitle: string;
+    description: string;
+  }>;
+  @Column({ type: 'jsonb', nullable: true })
+  images: Array<{
+    imageId: string;
+  }>;
+  @Column()
+  price: number;
+  @Column()
+  discount: number;
+  @Column()
+  priceAfterDiscount: number;
+  @Column()
+  like: number;
+  @Column()
+  comment: number;
+  @Column({ type: 'jsonb', nullable: true })
+  info: Array<{
+    name: string;
+    value: string;
+  }>;
+  @CreateDateColumn()
+  createdAt: Date;
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
