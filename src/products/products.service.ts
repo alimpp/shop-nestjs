3d22e53a27296562d83f4135a7d9282976568ed2
Alimpp <86446834+alimpp@users.noreply.github.com>
2025-10-06 11:30:46 +0330

    Update products.service.ts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductsEntity } from 'src/entities/products.entity';
+import { ProductsCommentEntity } from 'src/entities/productsComment.entity';
+import { ProductsLikeEntity } from 'src/entities/productsLike.entity';
+import { Repository } from 'typeorm';
+import { CreateDto } from './dto/create.dto';
+import { UpdateDto } from './dto/update.dto';
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(ProductsEntity)
+    private readonly productsRepository: Repository<ProductsEntity>,
+    @InjectRepository(ProductsLikeEntity)
+    private readonly productsLikeRepository: Repository<ProductsLikeEntity>,
+    @InjectRepository(ProductsCommentEntity)
+    private readonly productsCommentRepository: Repository<ProductsCommentEntity>,
+  ) {}
+
+  async getAll() {
+    return await this.productsRepository.find();
+  }
+
+  async findById(id: string) {
+    return await this.productsRepository.findOne({ where: { id } });
+  }
+
+  async add(body: CreateDto) {
+    const item = this.productsRepository.create(body);
+    return await this.productsRepository.save(item);
+  }
+
+  async update(id: string, body: UpdateDto) {
+    return await this.productsRepository.update(id, body);
+  }
+
+  async remove(id: string) {
+    await this.productsRepository.delete(id);
+  }
+
+  async getAllLike(productId: string) {
+    return await this.productsLikeRepository.find({ where: { productId } });
+  }
+
+  async findLikeById(body: { productId: string; likedBy: string }) {
+    return await this.productsLikeRepository.findOne({
+      where: {
+        productId: body.productId,
+        likedBy: body.likedBy,
+      },
+    });
+  }
+
+  async liked(body: { productId: string; likedBy: string }) {
+    const item = this.productsLikeRepository.create(body);
+    return await this.productsLikeRepository.save(item);
+  }
+
+  async disLike(id: string) {
+    await this.productsLikeRepository.delete(id);
+  }
+
+  async getAllComment(productId: string) {
+    return await this.productsCommentRepository.find({ where: { productId } });
+  }
+
+  async addComment(body: {
+    productId: string;
+    commentFrom: string;
+    comment: string;
+  }) {
+    const item = this.productsCommentRepository.create(body);
+    return await this.productsCommentRepository.save(item);
+  }
+
+  async removeComment(id: string) {
+    await this.productsCommentRepository.delete(id);
+  }
+}
