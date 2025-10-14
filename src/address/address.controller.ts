60bf39fba18472be71652bd628823bedc993b827
Alimp <A.Mohsenpour@sgas.ir>
2025-10-14 14:54:31 +0330

    New api

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,19 +1,19 @@
-import { 
-  Controller, 
-  Get, 
-  Post, 
-  Patch, 
-  Delete, 
-  Param, 
-  Body, 
-  UseGuards,
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
   Request,
-  NotFoundException
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
 import { AddressService } from './address.service';
 import { CreateDto } from './dto/create.dto';
 import { UpdateDto } from './dto/update.dto';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth/jwt-auth.guard';
 
 @Controller('address')
 @UseGuards(JwtAuthGuard)
@@ -22,44 +22,43 @@ export class AddressController {
 
   @Get('/all/admin')
   async getAllAddresses(@Request() req) {
-    if(req.user.role != 'admin') throw new NotFoundException('Not Found Exception')
+    if (req.user.role != 'admin')
+      throw new NotFoundException('Not Found Exception');
     return await this.addressService.getAllAddress();
-
   }
 
   @Get('/all/user')
-  async getUserAddresses(
-    @Request() req
-  ) {
+  async getUserAddresses(@Request() req) {
     return await this.addressService.getUserAddress(req.user.id);
   }
 
   @Post('/add')
-  async createAddress(
-    @Body() body: CreateDto,
-    @Request() req
-  ) {
-    return await this.addressService.add({...body, userId: req.user.id});
+  async createAddress(@Body() body: CreateDto, @Request() req) {
+    return await this.addressService.add({ ...body, userId: req.user.id });
+  }
+
+  @Get(':id')
+  async getAddressByUserId(@Param('id') id: string) {
+    return await this.addressService.getUserAddress(id);
   }
 
   @Patch(':id')
   async updateAddress(
     @Param('id') id: string,
     @Body() updateDto: UpdateDto,
-    @Request() req
+    @Request() req,
   ) {
     const address = await this.addressService.update(id, updateDto);
-    if(!address) throw new NotFoundException(`Address with id ${id} not found`) 
-    return address
+    if (!address)
+      throw new NotFoundException(`Address with id ${id} not found`);
+    return address;
   }
 
   @Delete(':id')
-  async deleteAddress(
-    @Param('id') id: string,
-    @Request() req
-  ) {
+  async deleteAddress(@Param('id') id: string, @Request() req) {
     const address = this.addressService.remove(id);
-    if(!address) throw new NotFoundException(`Address with id ${id} not found`)
-    return address 
+    if (!address)
+      throw new NotFoundException(`Address with id ${id} not found`);
+    return address;
   }
 }
