3a879f5e5700173daa5cff672709cc78dfb2dfa3
Alimp <86446834+alimpp@users.noreply.github.com>
2026-06-25 16:55:13 +0330

    clear source

--- dbConfig.ts
+++ dbConfig.ts
@@ -1,26 +1,6 @@
 import { AddressEntity } from 'src/entities/address.entity';
 import { AdminEntity } from 'src/entities/admin.entity';
-import { BannersEntity } from 'src/entities/banners.entity';
-import { BlogsEntity } from 'src/entities/blogs.entity';
-import { BlogsCommentEntity } from 'src/entities/blogsComment.entity';
-import { BlogsLikeEntity } from 'src/entities/blogsLike.entity';
-import { BrandsEntity } from 'src/entities/brand.entity';
-import { CategoryEntity } from 'src/entities/category.entity';
-import { CategoryHistoryEntity } from 'src/entities/categoryHistory.entity';
-import { ChatListEntity } from 'src/entities/chatList.entity';
 import { File } from 'src/entities/fileEntity';
-import { HeroEntity } from 'src/entities/hero.entity';
-import { NotificationEntity } from 'src/entities/notification.entity';
-import { ProductsEntity } from 'src/entities/products.entity';
-import { ProductsCommentEntity } from 'src/entities/productsComment.entity';
-import { ProductsLikeEntity } from 'src/entities/productsLike.entity';
-import { ProperttyEntity } from 'src/entities/propertty.entity';
-import { ProperttyHistoryEntity } from 'src/entities/properttyHistory.entity';
-import { ProperttyValueEntity } from 'src/entities/properttyValue.entity';
-import { ProperttyValueHistoryEntity } from 'src/entities/properttyValueHistory.entity';
-import { ServicesEntity } from 'src/entities/services.entity';
-import { SettingEntity } from 'src/entities/setting.entity';
-import { SupportEntity } from 'src/entities/support.entity';
 import { UserEntity } from 'src/entities/user.entity';
 
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
@@ -32,31 +12,6 @@ export const pgConfig: PostgresConnectionOptions = {
   username: 'postgres',
   password: 'js68002100',
   database: 'dynamic_shop',
-  entities: [
-    UserEntity,
-    File,
-    AdminEntity,
-    AddressEntity,
-    CategoryEntity,
-    CategoryHistoryEntity,
-    ProperttyEntity,
-    ProperttyHistoryEntity,
-    ProperttyValueEntity,
-    ProperttyValueHistoryEntity,
-    SupportEntity,
-    ChatListEntity,
-    NotificationEntity,
-    HeroEntity,
-    BrandsEntity,
-    BannersEntity,
-    ServicesEntity,
-    BlogsEntity,
-    BlogsLikeEntity,
-    BlogsCommentEntity,
-    ProductsEntity,
-    ProductsLikeEntity,
-    ProductsCommentEntity,
-    SettingEntity,
-  ],
+  entities: [UserEntity, File, AdminEntity, AddressEntity],
   synchronize: true,
 };
